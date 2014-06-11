28a58b3aade13cc1cd6b9dde0a3a0d63a6d2c1a5
Tal Giat <tal.giat@thomsonreuters.com>
2014-06-11 19:16:29 -0400

    adding latest om version

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "http://example.com/FIXME"
 
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2173"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
-                 [om "0.5.0"]]
+                 [org.clojure/clojurescript "0.0-2173"]
+                 [om "0.6.2"]]
 
   :plugins [[lein-cljsbuild "1.0.2"]]
 
